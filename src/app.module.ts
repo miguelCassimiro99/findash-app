3dfe7968fc7455ef3a2f1ea224a8716e7ed5e590
Miguel Cassimiro <miguel.cassimiro99@gmail.com>
2024-01-27 21:38:07 -0300

    feat: user crud and validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
